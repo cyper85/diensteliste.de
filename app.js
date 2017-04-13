7bb4d9d827eaceabaecc73c2ef0f9090e6704d86
Andreas Neumann <andr.neumann@googlemail.com>
2017-04-13 13:53:12 +0200

    Einbindung i18n und handlebars

--- app.js
+++ app.js
@@ -1,9 +1,82 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    cookieParser = require('cookie-parser'),
+    i18n = require('i18n'),
+    hbs = require('hbs'),
+    app = module.exports = express();
 
-app.get('/', function (req, res) {
-  console.log(req);
-  res.send('Hello World!');
+i18n.configure({
+  // setup some locales - other locales default to en silently
+  locales: ['de', 'en'],
+
+    // fall back from Dutch to German
+    fallbacks:{'en': 'de'},
+
+    // you may alter a site wide default locale
+    defaultLocale: 'de',
+
+  // sets a custom cookie name to parse locale settings from
+  cookie: 'locale',
+
+  // where to store json files - defaults to './locales'
+  directory: __dirname + '/lang'
+});
+
+  // setup hbs
+  app.set('views', "" + __dirname + "/views");
+  app.set('view engine', 'hbs');
+  app.engine('hbs', hbs.__express);
+  app.use(express.static(__dirname + '/public'));
+
+  // you'll need cookies
+  app.use(cookieParser());
+
+  // init i18n module for this loop
+  app.use(i18n.init);
+
+
+
+var blocks = {};
+
+// register hbs helpers in res.locals' context which provides this.locale
+hbs.registerHelper('__', function () {
+  return i18n.__.apply(this, arguments);
+});
+hbs.registerHelper('__n', function () {
+  return i18n.__n.apply(this, arguments);
+});
+
+hbs.registerHelper('language', function(name, context) {
+    return
+});
+
+hbs.registerHelper('extend', function(name, context) {
+    var block = blocks[name];
+    if (!block) {
+        block = blocks[name] = [];
+    }
+
+    block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
+});
+
+hbs.registerHelper('block', function(name) {
+    var val = (blocks[name] || []).join('\n');
+
+    // clear the block
+    blocks[name] = [];
+    return val;
+});
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+app.get('/imprint', function(req, res){
+    res.render('index',{title: "Imprint"});
+});
+
+app.get('/lang/:locale', function (req, res) {
+  res.cookie('locale', req.params.locale);
+  i18n.setLocale(i18n, req.params.locale);
+  res.redirect(req.headers.referer);
 });
 
 app.listen(3000, function () {
